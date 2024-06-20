f1cfb7dd69fa595a148b57494905bed20c944ec6
shaosenwang <shaosenwang@tuputech.com>
2024-06-20 19:23:44 +0800

    fix(null): vote-only & pretty_names

--- views/core.py
+++ views/core.py
@@ -78,7 +78,7 @@ class Core(object):
                             ft.DataRow(cells=[ft.DataCell(S_Text("允许主节点数")),
                                               ft.DataCell(S_Text(f"{stats_nodes['count']['master']}"))]),
                             ft.DataRow(cells=[ft.DataCell(S_Text("仅投票节点数")),
-                                              ft.DataCell(S_Text(f"{stats_nodes['count']['voting_only']}"))]),
+                                              ft.DataCell(S_Text(f"{stats_nodes['count'].get('voting_only')}"))]),
                         ], column_spacing=10, ), ),
                         ft.Card(ft.DataTable(columns=[
                             ft.DataColumn(S_Text("索引、分片", weight=ft.FontWeight.BOLD)), ft.DataColumn(S_Text("")),
@@ -157,8 +157,8 @@ class Core(object):
                             ft.DataRow(cells=[ft.DataCell(S_Text("分配给ES进程使用的CPU核心数")),
                                               ft.DataCell(S_Text(f"{stats_nodes['os']['allocated_processors']}")), ]),
                             ft.DataRow(cells=[ft.DataCell(S_Text("操作系统及总数")), ft.DataCell(
-                                ft.Text(value=json.dumps(f"{stats_nodes['os']['pretty_names']}")[:10] + "...",
-                                        tooltip=json.dumps(f"{stats_nodes['os']['pretty_names']}"))), ]),
+                                ft.Text(value=json.dumps(f"{stats_nodes['os'].get('pretty_names')}")[:10] + "...",
+                                        tooltip=json.dumps(f"{stats_nodes['os'].get('pretty_names')}"))), ]),
                             ft.DataRow(cells=[ft.DataCell(S_Text("总内存")), ft.DataCell(
                                 S_Text(f"{human_size(stats_nodes['os']['mem']['total_in_bytes'])}")), ]),
                             ft.DataRow(cells=[ft.DataCell(S_Text("已使用内存")), ft.DataCell(
